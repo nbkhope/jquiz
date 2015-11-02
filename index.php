9bcb20c34cce218f370a91787b28a79533b509be
nbkhope <ryumemaru@yahoo.com>
2015-11-01 20:16:01 -0800

    Reverts back to initial commit

--- index.php
+++ index.php
@@ -1,26 +1,23 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta name="robots" content="index, nofollow">
-	<meta name="description" content="Practice for the JLPT by taking a quiz">
-	<meta name="keywords" content="JLPT, test, Japanese, language, practice, Japan">
-	<meta name="author" content="Renan Martins">
-	
-	<title>jQuiz</title>
-	
-	<!-- CSS -->
-	<link rel="stylesheet" type="text/css" href="stylesheet.css">
-	<!-- jQuery -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-	
-	<script>
-	$(document).ready(function() {
-		$("#lesson-menu").click(function() {
-			$(".lesson-menu").toggle();
-		});
+<meta charset="utf-8">
+<meta name="robots" content="index, nofollow">
+<meta name="description" content="Practice for the JLPT by taking a quiz">
+<meta name="keywords" content="JLPT, test, Japanese, language, practice, Japan">
+<meta name="author" content="Renan Martins">
+<title>jQuiz</title>
+<!-- CSS -->
+<link rel="stylesheet" type="text/css" href="stylesheet.css">
+<!-- jQuery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script>
+$(document).ready(function() {
+	$("#lesson-menu").click(function() {
+		$(".lesson-menu").toggle();
 	});
-	</script>
+});
+</script>
 </head>
 <body>
 	
@@ -39,23 +36,24 @@ $qbank->fetchLessons();
 
 $qbank->init();
 
+//$qbank->fetchQuestionsForLesson();
 ?>
 
 <header class="site-header">
-	<span class="site-title"><?php echo SITE_TITLE; ?></span>
+<span class="site-title">jQuiz</span>
 </header>	
 
 <nav class="site-menu">
-	<ul>
-		<li><a href="index.php">Home</a></li>
-		<?php if (isset($_GET['lesson'])) { ?>
-		<li><span id="lesson-menu" style="cursor:pointer;">Lessons</span></li>
-		<?php } ?>
-		<li><a href="index.php?reset">Reset</a></li>
-	</ul>
-	<div class="lesson-menu">
-	<?php $qbank->getLessons(); ?>
-	</div>
+<ul>
+<li><a href="index.php">Home</a></li>
+<?php if (isset($_GET['lesson'])) { ?>
+<li><span id="lesson-menu" style="cursor:pointer;">Lessons</span></li>
+<?php } ?>
+<li><a href="index.php?reset">Reset</a></li>
+</ul>
+<div class="lesson-menu">
+<?php $qbank->getLessons(); ?>
+</div>
 </nav>
 
 <section class="site-content">
@@ -64,32 +62,9 @@ $qbank->init();
 
 if (isset($_GET['lesson']) && !empty($_GET['lesson'])) {
 
-	// If questions are already stored in the session, it will not
-	// query from the database. Otherwise, for that or for a different
-	// lesson (from the current lesson), the questions will be reloaded
-	// from the database
-	if (isset($_SESSION['questions']) && !empty($_SESSION['questions'])) {
-		if ($_GET['lesson'] !== $_SESSION['current_lesson']) {
-			
-			$qbank->current_lesson = $_GET['lesson'];
-			$_SESSION['current_lesson'] = $qbank->current_lesson;
-			
-			$qbank->fetchQuestionsForLesson();
-			
-			$qbank->setSessionQuestionsArray($_SESSION['questions']);
-		}
-		else {
-			$qbank->fetchQuestionsForLessonUseSession();
-		}
-	}
-	else {
-		$qbank->current_lesson = $_GET['lesson'];
-		$_SESSION['current_lesson'] = $qbank->current_lesson;
-		
-		$qbank->fetchQuestionsForLesson();
-		
-		$qbank->setSessionQuestionsArray($_SESSION['questions']);
-	}
+	$qbank->fetchQuestionsForLesson();
+
+	$qbank->current_lesson = $_GET['lesson'];
 
 	echo "<h1>Lesson " . $qbank->current_lesson . "</h1>";
 
@@ -110,7 +85,7 @@ if (isset($_GET['lesson']) && !empty($_GET['lesson'])) {
 	echo '</div><!-- .question-area -->';
 }
 else {
-	echo "<h1>" . SITE_SUBTITLE . "</h1>";
+	echo "<h1>Studying for the JLPT</h1>";
 	$qbank->getLessons();
 }
 

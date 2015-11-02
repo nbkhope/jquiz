7807ec46826dd4154fabcf40859ed1734249076f
nbkhope <ryumemaru@yahoo.com>
2015-11-01 20:10:27 -0800

    Reverts questionbank.sql to initial version and updates question list with new questions and corrections to previous questions.

--- sql/questionbank.sql
+++ sql/questionbank.sql
@@ -5,15 +5,11 @@
 DROP TABLE IF EXISTS `questionbank`;
 
 CREATE TABLE IF NOT EXISTS `jquiz`.`questionbank` (
-	id INT NOT NULL AUTO_INCREMENT,
-	level INT(11) NOT NULL,
 	lesson INT(11) NOT NULL,
 	question VARCHAR(512) NOT NULL DEFAULT "Unknown Question",
 	choice_a VARCHAR(512) NOT NULL DEFAULT "Unknown Answer",
 	choice_b VARCHAR(512),
 	choice_c VARCHAR(512),
 	choice_d VARCHAR(512),
-	answer CHAR(1) NOT NULL DEFAULT 'a',
-	PRIMARY KEY (id)
+	answer CHAR(1) NOT NULL DEFAULT 'a'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-

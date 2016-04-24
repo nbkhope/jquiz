52d37259e51e6faf3eb6a876551e9dd2470badc2
nbkhope <ryumemaru@yahoo.com>
2016-04-24 11:51:00 -0700

    Add table creation MySQL statements for lessons, questions, and responses

--- sql/questionbank.sql
+++ sql/questionbank.sql
@@ -3,6 +3,10 @@
  */
 
 DROP TABLE IF EXISTS `questionbank`;
+DROP TABLE IF EXISTS `responses`;
+DROP TABLE IF EXISTS `questions`;
+DROP TABLE IF EXISTS `lessons`;
+
 
 CREATE TABLE IF NOT EXISTS `jquiz`.`questionbank` (
 	lesson INT(11) NOT NULL,
@@ -12,4 +16,27 @@ CREATE TABLE IF NOT EXISTS `jquiz`.`questionbank` (
 	choice_c VARCHAR(512),
 	choice_d VARCHAR(512),
 	answer CHAR(1) NOT NULL DEFAULT 'a'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `lessons` (
+	id INT NOT NULL AUTO_INCREMENT,
+	number INT NOT NULL,
+	PRIMARY KEY(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `questions` (
+	id INT NOT NULL AUTO_INCREMENT,
+	prompt VARCHAR(256) NOT NULL,
+	lesson_id INT,
+	PRIMARY KEY(id),
+	FOREIGN KEY(lesson_id) REFERENCES lessons(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `responses` (
+	id INT NOT NULL AUTO_INCREMENT,
+	prompt VARCHAR(256) NOT NULL,
+	is_right_answer BOOLEAN,
+	question_id INT,
+	PRIMARY KEY(id),
+	FOREIGN KEY(question_id) REFERENCES questions(id)	
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;

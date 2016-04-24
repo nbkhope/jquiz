6fa55e514a61955fe6e3e977c8700d174fbfec79
nbkhope <ryumemaru@yahoo.com>
2016-04-24 14:09:20 -0700

    Fix bug where id field would not be properly computed for the responses.csv file generation

--- sql/split_questionbank_csv.rb
+++ sql/split_questionbank_csv.rb
@@ -5,6 +5,8 @@ question_prompts = []
 responses = []
 correct_answers = []
 
+puts "Parsing original CSV file . . ."
+
 # Parse the CSV file
 CSV.foreach('QuestionBank.csv', col_sep: "\t") do |row|
 	#p row
@@ -23,15 +25,19 @@ CSV.foreach('QuestionBank.csv', col_sep: "\t") do |row|
 	responses << responses_description
 	correct_answers << correct_answer
 
-	puts "Got: #{lesson_number}, #{question_prompt}, #{responses_description.join(",")}, #{correct_answer}"
+	#puts "Got: #{lesson_number}, #{question_prompt}, #{responses_description.join(",")}, #{correct_answer}"
 end
 
-p lesson_numbers
-p question_prompts
-p responses
-p correct_answers
+# p lesson_numbers
+# p question_prompts
+# p responses
+# p correct_answers
 
 unique_lesson_numbers = lesson_numbers.uniq
+
+puts "Generating new CSV files . . ."
+
+puts "Creating lessons.csv"
 
 # Generate lessons.csv
 index = 0
@@ -42,24 +48,27 @@ CSV.open("lessons.csv", "wb") do |csv|
 	end
 end
 
+puts "Creating questions.csv"
+
 # Generate questions.csv
 index = 0
 CSV.open("questions.csv", "wb") do |csv|
 	while index < question_prompts.length
-	  csv << [index + 1, question_prompts[index], unique_lesson_numbers.index(lesson_numbers[index]) + 1]
+	  csv << [index + 1, question_prompts[index], unique_lesson_numbers.index(lesson_numbers[index]) + 1	]
 	  index += 1
 	end
 end
 
-# Generate responses.csv
+puts "Creating responses.csv"
 
+# Generate responses.csv
 id_index = 0
-count = 0
+count = 1
 CSV.open("responses.csv", "wb") do |csv|
 	while id_index < responses.length
 		correct_answer_as_index = correct_answers[id_index].downcase.codepoints.first - 97
 		
-		responses[id_index].each.with_index(id_index + count) do |response, response_index|
+		responses[id_index].each.with_index do |response, response_index|
 			# Determine whether the current response is the right answer to the question
 			if response_index == correct_answer_as_index
 				is_right_choice = 1 
@@ -67,10 +76,12 @@ CSV.open("responses.csv", "wb") do |csv|
 				is_right_choice = 0
 			end
 
-			csv << [response_index + 1, response, id_index + 1, is_right_choice]
-			count += 1
+			csv << [count, response, id_index + 1, is_right_choice]
+			count += 1 
 	  end
 
 	  id_index += 1
 	end
 end
+
+puts "[Complete]"
